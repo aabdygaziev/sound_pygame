d631c6c99f76e2e16b2e21b08a2ef4635d52238b
aabdygaziev <aabdygaziev@gmail.com>
2019-01-08 17:37:25 +0600

    test

---
+++ playsound.py
@@ -0,0 +1,6 @@
+import pygame
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load('20 Syllables (feat. Jay-Z, Dr. Dre, 50 Cent, Stat Quo & Cashis).mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
